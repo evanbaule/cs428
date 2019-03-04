aae709fdec14ba816ccc8e0aab755ade0d77cbe1
Evan Baule <evanbaule@Evans-MacBook-Pro-2.local>
2019-03-03 19:13:46 -0500

    this is not going very well

--- proj1/server.c
+++ proj1/server.c
@@ -1,15 +1,22 @@
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-
+#include <assert.h>
 
 int
 main(int argc, char const ** argv)
 {
     //syscall create socket TCP/IPv4
-    int socket_fd = socket(PF_INET, SOCK_STREAM, 0);
-    printf("sfd:\t%d\n", socket_fd);
+    int ss_fd = socket(PF_INET, SOCK_STREAM, 0); assert(ss_fd != -1 && "Socket creation syscall failure.");
+    printf("socket_fd:\t%d\n", ss_fd);
+
+    //syscall setsockopt to modify socket construction
+    //int sso_rv = setsockopt(ss_fd, ); assert(sso_rv != -1 && "Setsockopt syscall failure to modify socket properties.");
+
+    struct socket_addr s_addr;
+    //syscall bind socket to 
 
     return 0;
 }
