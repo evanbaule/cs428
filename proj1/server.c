90f0aa6c59162501d8273059aafccaebcb8d9942
Evan Baule <evanbaule@Evans-MacBook-Pro-2.local>
2019-03-04 12:42:04 -0500

    this shit is so annoying

--- proj1/server.c
+++ proj1/server.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
 #include <assert.h>
 
 int
@@ -15,8 +16,12 @@ main(int argc, char const ** argv)
     //syscall setsockopt to modify socket construction
     //int sso_rv = setsockopt(ss_fd, ); assert(sso_rv != -1 && "Setsockopt syscall failure to modify socket properties.");
 
-    struct socket_addr s_addr;
+    struct socketaddr_in *saddr;
+    saddr->sin_family = PF_INET;
+    saddr->sin_port = htons(8080);
+    //saddr->sin_addr = htonl(INADDR_ANY);
     //syscall bind socket to 
+    int brv = bind(ss_fd, saddr, sizeof(saddr)); assert(brv != -1 && "Failure binding socket to port: 8080");
 
     return 0;
 }
