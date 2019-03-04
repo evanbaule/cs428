65139872bb781d5a7e9a9ddd76bdfe72b275dd86
Evan Baule <evanbaule@Evans-MacBook-Pro-2.local>
2019-03-04 14:10:36 -0500

    some work done ig uess

--- proj1/server.c
+++ proj1/server.c
@@ -6,27 +6,36 @@
 #include <netinet/in.h>
 #include <assert.h>
 #include <errno.h>
+#include <string.h>
 
 int
 main(int argc, char const ** argv)
 {
     //syscall create socket TCP/IPv4
-    int ss_fd = socket(PF_INET, SOCK_STREAM, 0); assert(ss_fd != -1 && "Socket creation syscall failure.");
-    printf("socket_fd:\t%d\n", ss_fd);
+    int serv_sfd = socket(PF_INET, SOCK_STREAM, 0); assert(serv_sfd != -1 && "Socket creation syscall failure.");
+    printf("socket_fd:\t%d\n", serv_sfd);
 
     //syscall setsockopt to modify socket construction
     //int sso_rv = setsockopt(ss_fd, ); assert(sso_rv != -1 && "Setsockopt syscall failure to modify socket properties.");
 
-    struct sockaddr_in* saddr;
-    saddr->sin_family = PF_INET;
-    saddr->sin_port = htons(8080);
-    //saddr->sin_addr = htonl(INADDR_ANY);
-    //syscall bind socket to 
-    int brv = bind(ss_fd, (struct sockaddr *)saddr, sizeof(saddr) < 0);
+    struct sockaddr_in serv_addr;
+    serv_addr.sin_family = PF_INET;
+
+    //Server will listen to 8080, clients will connect to localhost:8080 to send to server
+    int serv_port = 8080;
+    serv_addr.sin_port = htons(serv_port);
+
+    //Set IP 0.0.0.0/ANY
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    //Zero out memory for safety
+    memset((char *)&serv_addr, 0, sizeof(serv_addr));
+    int brv = bind(serv_sfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr) < 0); //assert(brv != -1 && "Failure binding to port: 8080");
     if(brv == -1)
     {
-      printf("we fucked it:\t%d\n", brv);
-      perror("we fucked it heres the errno");
+        printf("brv@fail:\t%d\n", brv);
+        perror("Failure binding socket to port: 8080");
+        exit(EXIT_FAILURE);
     }
 
     return 0;
