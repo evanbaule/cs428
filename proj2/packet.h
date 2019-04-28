a5a6fc98505c810417ba2e463afed7a7bea405ff
Evan Baule <evanbaule@Evans-MacBook-Pro-3.local>
2019-04-28 19:04:53 -0400

    added a whole bunch of stuff for p2

---
+++ proj2/packet.h
@@ -0,0 +1,27 @@
+#define PACKET_SIZE 1500
+
+typedef struct packet
+{
+
+} __attribute__((packed)) packet;
+
+typedef struct packet_meta
+{
+    short op_code; //01
+    char file_name[32]; //max length set to ensure fixed size, tells name of inbound file
+    int file_size;  //tells size of inbound file
+    int num_packets; // total # of packets for the entire file (might as well send it with all this space)
+} __attribute__ ((packed)) packet_meta;
+
+typedef struct packet_datagram
+{
+    short op_code; //02
+    int packet_num;
+    char data[1494]; //1500 - 6 for the op code and packet number
+} __attribute__((packed)) packet_datagram;
+
+typedef struct packet_ack
+{
+    short op_code; //03
+    char file_name[32]; //might as well use the space
+} __attribute__((packed)) packet_ack;
